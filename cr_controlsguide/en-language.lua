a895b4fd2e30c39692b8753f8cfe4e3a1af7d5a9
CriticalR <145730435+CriticalR@users.noreply.github.com>
2023-09-23 17:22:10 +0200

    Add files via upload

---
+++ cr_controlsguide/en-language.lua
@@ -0,0 +1,11 @@
+---
+--- @Author CriticalR, crcloud-fr
+--- @Copyright (c) CriticalR & crcloud-fr - All Rights Reserved
+--- @DateTime: 23/09/2023 17:20:44
+---
+
+Strings = {
+    key_description = "Open Controls Guide Menu",
+    menu_title = "Controls Guide",
+    menu_description = "Take a look at our keybinds"
+}
---
+++ cr_controlsguide/fr-language.lua
@@ -0,0 +1,11 @@
+---
+--- @Author CriticalR, crcloud-fr
+--- @Copyright (c) CriticalR & crcloud-fr - All Rights Reserved
+--- @DateTime: 23/09/2023 17:20:48
+---
+
+Strings = {
+    key_description = "Ouvrir le menu des touches",
+    menu_title = "Controls Guide",
+    menu_description = "Jeter un coup d'œil à nos raccourcies"
+}
