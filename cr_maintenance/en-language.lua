aef1268bcbd59d3e369032f9e894d73b8f4f1fab
CriticalR <145730435+CriticalR@users.noreply.github.com>
2023-10-03 20:09:53 +0200

    Update en-language.lua

--- cr_maintenance/en-language.lua
+++ cr_maintenance/en-language.lua
@@ -6,9 +6,12 @@
 
 
 Strings = {
-    start_recording_and_save = "Start recording and save",
-    open_rockstar_editor = "Open rockstart editor",
-    start_recording = "Start recording",
-    stop_record_save = "Stop recording and save",
-    stop_record_discard = "Stop recording and Discard",
+    drop_message  = "The server is currently undergoing maintenance. Please try again later. For more information, please visit our Discord server: %s.",
+    cmd_maintenance_state_on = "The server is currently in maintenance mode.",
+    cmd_maintenance_state_off = "The server is currently in public mode.",
+    is_player_allowed = "Allowed connection: ^3%s^7.",
+    loaded_files = "Loaded maintenance whitelist data.",
+    add_player_in_whitelist = "Added [%s] to the maintenance whitelist.",
+    connecting = "Connecting: ^3%s^7.",
+    dropping_player_connection = "Dropping: ^3%s^7, Reason: ^3Not Whitelisted^7."
 }
