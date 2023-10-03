9b569829cf6df067a72aed5dd30b0f2fa8682b06
CriticalR <145730435+CriticalR@users.noreply.github.com>
2023-10-03 20:09:01 +0200

    Add files via upload

---
+++ cr_maintenance/fr-language.lua
@@ -0,0 +1,14 @@
+---
+--- @Author CriticalR, crcloud-fr
+--- @Copyright (c) CriticalR & crcloud-fr - All Rights Reserved
+--- @DateTime: 23/09/2023 15:57:23
+---
+
+
+Strings = {
+    start_recording_and_save = "Démarrer l’enregistrement et enregistrer",
+    open_rockstar_editor = "Ouvrir le rockstart editeur",
+    start_recording = "Démarrer l’enregistrement",
+    stop_record_save = "Arrêter l’enregistrement et enregistrer",
+    stop_record_discard = "Arrêter l’enregistrement et l'abandonner",
+}
